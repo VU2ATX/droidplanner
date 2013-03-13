2c0b392cf14b58d9a85fdd5d9322392317ea613c
Arthur Benemann <arthur.benemann@gmail.com>
2013-03-12 21:50:30 -0300

    Changing drone marker to an LocationSource.
    This way the memory heap grow problem is avoided.

--- DroidPlanner/src/com/diydrones/droidplanner/fragments/FlightMapFragment.java
+++ DroidPlanner/src/com/diydrones/droidplanner/fragments/FlightMapFragment.java
@@ -1,7 +1,6 @@
 package com.diydrones.droidplanner.fragments;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,28 +8,22 @@ import android.view.ViewGroup;
 
 import com.MAVLink.Messages.MAVLinkMessage;
 import com.MAVLink.Messages.ardupilotmega.msg_global_position_int;
-import com.diydrones.droidplanner.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.UiSettings;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FlightMapFragment extends MapFragment {
-	private Bitmap planeBitmap;
+public class FlightMapFragment extends MapFragment implements LocationSource{
 	private GoogleMap mMap;
-	private Marker DroneMarker;
+	private OnLocationChangedListener myLocationListner;
+	Location droneLocation;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup,
 			Bundle bundle) {
 		View view = super.onCreateView(inflater, viewGroup, bundle);
-		planeBitmap = BitmapFactory.decodeResource(getResources(),
-				R.drawable.planetracker);
-
+		
 		mMap = getMap();
 		mMap.setMyLocationEnabled(true);
 		mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
@@ -39,38 +32,19 @@ public class FlightMapFragment extends MapFragment {
 		mUiSettings.setMyLocationButtonEnabled(true);
 		mUiSettings.setCompassEnabled(true);
 		mUiSettings.setTiltGesturesEnabled(false);
-		
-		addDroneMarkerToMap();
+		mMap.setLocationSource(this);
 
+		droneLocation = new Location("");
+		droneLocation.setAccuracy(10);
 		return view;
 	}
 
 	public void updateDronePosition(float heading, LatLng coord) {
-		DroneMarker.setPosition(coord); // TODO This causes the heap to grow a lot.
-		if(!DroneMarker.isVisible()){
-			DroneMarker.setVisible(true);
-			mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coord, 16));
-		}
-	}
-
-	private void addDroneMarkerToMap() {
-			// TODO Find a way to rotate the plane that doesn't consume too much CPU power
-			/*Matrix matrix = new Matrix();
-			matrix.postRotate(heading - mMap.getCameraPosition().bearing);
-			Bitmap rotatedPlane = Bitmap.createBitmap(planeBitmap, 0, 0,
-					planeBitmap.getWidth(), planeBitmap.getHeight(), matrix,
-					true);
-					
-			mMap.addMarker(new MarkerOptions().position(coord)
-					.anchor((float) 0.5, (float) 0.5)
-					.icon(BitmapDescriptorFactory.fromBitmap(rotatedPlane)));
-			*/
-			DroneMarker = mMap.addMarker(new MarkerOptions()
-					.anchor((float) 0.5, (float) 0.5)
-					.position(new LatLng(0, 0))
-					.icon(BitmapDescriptorFactory.fromBitmap(planeBitmap))
-					.visible(false));
-			
+		droneLocation.setLatitude(coord.latitude);
+		droneLocation.setLongitude(coord.longitude);
+		droneLocation.setBearing(heading);
+		
+		myLocationListner.onLocationChanged(droneLocation);
 	}
 
 	public void receiveData(MAVLinkMessage msg) {
@@ -79,6 +53,15 @@ public class FlightMapFragment extends MapFragment {
 			float heading = (0x0000FFFF & ((int)((msg_global_position_int)msg).hdg))/100f; // TODO fix unsigned short read at mavlink library
 			updateDronePosition(heading, position);
 		}
+	}
+
+	@Override
+	public void activate(OnLocationChangedListener listner) {
+		myLocationListner  = listner;		
+	}
+
+	@Override
+	public void deactivate() {		
 	}
 
 }
